6719649fd0eaf696a2ec31027dbdb1e13a666fc0
Chanyub.Park <mrchypark@gmail.com>
2020-06-15 17:02:24 +0000

    ky work  check done

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,9 @@
 import { exec } from "./deps.ts";
+import ky from 'https://deno.land/x/ky/index.js';
+
+const parsed = await ky.get('https://httpbin.org/get').json();
+console.log(parsed)
 
 const tem = ["tems","tes"]
-
 console.log(tem.map(val => val + " is good").join(" + "))
-
 await exec('kubectl version');
