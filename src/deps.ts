2905f99e4951b80fec9623342100ed533798ca99
Chanyub.Park <mrchypark@gmail.com>
2020-06-17 03:31:45 +0900

    add print

--- src/deps.ts
+++ src/deps.ts
@@ -2,4 +2,3 @@ export { encode } from "https://deno.land/std/encoding/base64.ts"
 export { exec } from "https://deno.land/x/exec@0.0.5/mod.ts"
 export { soxa } from 'https://deno.land/x/soxa@v1.0/mod.ts'
 export { cron } from 'https://deno.land/x/deno_cron/cron.ts';
-export { format } from 'https://deno.land/x/date_fns/format/index.js';
