fc2e33003ca1f3cf6680b8834f94152efc94c215
Chanyub.Park <mrchypark@gmail.com>
2020-06-16 16:47:55 +0000

    add cron

--- src/mod.ts
+++ src/mod.ts
@@ -1,12 +1,11 @@
-import { exec, soxa, encode } from "./deps.ts";
+import { exec, soxa, encode, cron } from "./deps.ts";
 
 const orgs = Deno.env.get("ORGS");
 const proj = Deno.env.get("PROJECT");
 const auth_key = Deno.env.get("KEY");
+const schedule = Deno.env.get("CRON_SCHEDULE") || "*/1 * * * *"
 const pat = `:` + auth_key;
 const env_name = Deno.env.get("ENV_NAME");
-
-// await exec('kubectl version');
 
 const vg_config = {
   baseURL: `https://dev.azure.com/${orgs}/${proj}/`,
@@ -15,25 +14,28 @@ const vg_config = {
   },
 };
 
-const parsed = await soxa.get(
-  `_apis/distributedtask/variablegroups?api-version=5.1-preview.1`,
-  vg_config
-);
 
-parsed.data.value
-  .filter((val: any) => val.name.includes(`${env_name}`))
-  .map(
-    (val: any) =>
-      `kubectl create configmap ${val.name.replace(
-        `-${env_name}`,
-        ""
-      )}${Object.entries(val.variables)
-        .map(
-          ([key, value]: [string, any]) =>
-            ` --from-literal=${key}=${value["value"]}`
-        )
-        .join("")} -o yaml --dry-run=client`
-  )
-  .map(async (val: string) => {
-    console.log(await exec(`bash -c "${val} | kubectl apply -f -"`));
-  });
+cron(schedule, () => {
+  let parsed = await soxa.get(
+    `_apis/distributedtask/variablegroups?api-version=5.1-preview.1`,
+    vg_config
+  );
+  
+  parsed.data.value
+    .filter((val: any) => val.name.includes(`${env_name}`))
+    .map(
+      (val: any) =>
+        `kubectl create configmap ${val.name.replace(
+          `-${env_name}`,
+          ""
+        )}${Object.entries(val.variables)
+          .map(
+            ([key, value]: [string, any]) =>
+              ` --from-literal=${key}=${value["value"]}`
+          )
+          .join("")} -o yaml --dry-run=client`
+    )
+    .map(async (val: string) => {
+      console.log(await exec(`bash -c "${val} | kubectl apply -f -"`));
+    });
+}
